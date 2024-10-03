a3cfc67aec17a2b07cfefb85d2e41383e670d10a
Shntnu45 <shantanuchaudhari9221@gmail.com>
2024-10-04 01:00:19 +0530

    Updates

---
+++ precache-manifest.0db0204ade59595763d648d1dd77f03b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba49eb3adc9e5e9e9ce23fcab483a7c7",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "32e37ad7885fe8786032",
+    "url": "/Portfolio/static/css/main.8ce462b2.chunk.css"
+  },
+  {
+    "revision": "485c3c9a2439378ac1b5",
+    "url": "/Portfolio/static/js/2.533bcd98.chunk.js"
+  },
+  {
+    "revision": "b95080709ac12d0967db165d90e6c04a",
+    "url": "/Portfolio/static/js/2.533bcd98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb931af923c5043c48b6",
+    "url": "/Portfolio/static/js/3.d520b6e6.chunk.js"
+  },
+  {
+    "revision": "32e37ad7885fe8786032",
+    "url": "/Portfolio/static/js/main.5ef25158.chunk.js"
+  },
+  {
+    "revision": "f72ac8a7b2faf5b7728d",
+    "url": "/Portfolio/static/js/runtime-main.9eff1c21.js"
+  },
+  {
+    "revision": "bbde04a95b206c6fcc6c1981b77e488f",
+    "url": "/Portfolio/static/media/Coder.bbde04a9.png"
+  },
+  {
+    "revision": "c7ac7aa3f0f08ca8f5027c6638a66db7",
+    "url": "/Portfolio/static/media/IMG_6305.c7ac7aa3.GIF"
+  },
+  {
+    "revision": "e8d1a63c5708e7dd1000d2a8e3f8377b",
+    "url": "/Portfolio/static/media/Navbarlogo.e8d1a63c.png"
+  },
+  {
+    "revision": "e966005f2a56295abd1a0b46dfbddb3f",
+    "url": "/Portfolio/static/media/Screenshot (173).e966005f.png"
+  },
+  {
+    "revision": "2d44d8efe0701cba2a2179a654ae3af5",
+    "url": "/Portfolio/static/media/Screenshot (174).2d44d8ef.png"
+  },
+  {
+    "revision": "4fbe9963803e314c030ab10d9568e2dc",
+    "url": "/Portfolio/static/media/Screenshot (175).4fbe9963.png"
+  },
+  {
+    "revision": "27746d42875858316669d5965b22c758",
+    "url": "/Portfolio/static/media/Tailwind_CSS_Logo.svg.27746d42.png"
+  },
+  {
+    "revision": "79953159bbf288952c4640e611853f4a",
+    "url": "/Portfolio/static/media/express-removebg-preview.79953159.png"
+  },
+  {
+    "revision": "ccd00340c0e81ef853844f478a7893e8",
+    "url": "/Portfolio/static/media/mongo.ccd00340.png"
+  }
+]);
